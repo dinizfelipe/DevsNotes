727df8896aa59557a1e176caf5e815d2850a3455
Felipe Diniz <paladiows@gmail.com>
2020-10-13 21:23:54 -0300

    Tela de edição - parte 1

--- src/pages/EditNoteScreen/index.js
+++ src/pages/EditNoteScreen/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   Container,
-  Texto
+  TitleInput,
+  BodyInput
 } from './styles';
 
 export default () => {
+
+  const [title, setTitle] = useState();
+  const [body, setBody] = useState();
+
   return (
     <Container>
-      <Texto>TELA DE EDITAR</Texto>
+      <TitleInput
+        value={title}
+        onChangeText={e => setTitle(e)}
+        placeholder="Título da anotação"
+        placeholderTextColor="#CCC"
+        autoFocus={true}
+      />
+      <BodyInput
+        value={body}
+        onChangeText={e => setBody(e)}
+        placeholder="O que deseja anotar?"
+        placeholderTextColor="#CCC"
+        multiline={true}
+        textAlignVertical="top"
+      />
     </Container>
   )
 }
--- src/pages/EditNoteScreen/styles.js
+++ src/pages/EditNoteScreen/styles.js
@@ -1,5 +1,20 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  background-color: #333;
+`;
 
-export const Texto = styled.Text``;
+export const TitleInput = styled.TextInput`
+  padding: 15px;
+  font-size: 22px;
+  color: #FFF
+  font-weight: bold;
+`;
+
+export const BodyInput = styled.TextInput`
+  flex: 1;
+  padding: 15px;
+  font-size: 17px;
+  color: #FFF;
+`;
