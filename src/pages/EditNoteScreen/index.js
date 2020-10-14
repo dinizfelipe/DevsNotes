5c8abf7be29e5cee9999ee1b4e631e7ba399d435
Felipe Diniz <paladiows@gmail.com>
2020-10-13 21:40:52 -0300

    Tela de edição - parte 2

--- src/pages/EditNoteScreen/index.js
+++ src/pages/EditNoteScreen/index.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigation, useRoute } from '@react-navigation/native'
 
 import {
   Container,
@@ -7,9 +9,22 @@ import {
 } from './styles';
 
 export default () => {
+  const navigation = useNavigation();
+  const route = useRoute();
+  const dispatch = useDispatch();
+  const list = useSelector(state => state.notes.list);
 
   const [title, setTitle] = useState();
   const [body, setBody] = useState();
+  const [status, setStatus] = useState('new');
+
+  useEffect(() => {
+    if (route.params?.key != undefined && list[route.params.key]) {
+      setStatus('edit');
+      setTitle(list[route.params.key].title)
+      setBody(list[route.params.key].body)
+    }
+  }, [])
 
   return (
     <Container>
